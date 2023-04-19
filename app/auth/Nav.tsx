ce5a3771837257c5f59945bce848877b433abf81
kmilgrim <kmilgrim@bu.edu>
2023-04-18 22:25:16 -0400

    this finally kinda works. had to downgrade next,js due to a bug

---
+++ app/auth/Nav.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link"
+import Login from "./Login"
+
+export default async function Nav(){
+    return  ( 
+        <nav>
+            <Link href={'/'}>
+            <h1>Send it!</h1>
+            </Link>
+            <ul>
+                <Login />
+            </ul>
+        </nav>
+    )
+}
---
+++ app/layout.jsx
@@ -0,0 +1,21 @@
+import './globals.css'
+import Nav from './auth/Nav'
+import { Roboto } from "@next/font/google"
+
+const roboto = Roboto({
+  subsets: ["latin"], 
+  weight: ["400", "700"],
+  variable: "--font-roboto",
+})
+
+export default function RootLayout({ children }) {
+
+  return (
+    <html lang="en">
+      <body className={`mx-4 md:mx:48 xl:mx-96 ${roboto.variable} bg-grey-20`}>
+        <Nav />
+        {children}
+        </body>
+    </html>
+  )
+}
