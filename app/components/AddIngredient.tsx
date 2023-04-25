9a53f9c13eca335b4620ac5c7557c132c63f6461
kmilgrim <kmilgrim@bu.edu>
2023-04-25 19:39:28 -0400

    Can add an ingredient, now need to work on making the ingredient object contain all the different inputs we need from the user.

--- app/components/AddIngredient.tsx
+++ app/components/AddIngredient.tsx
@@ -1,52 +1,57 @@
-'use client' 
- 
-import { useState } from "react" 
-import { useMutation, useQueryClient } from '@tanstack/react-query' 
-import axios from "axios" 
- 
-export default function AddIngredient(){ 
-    const [ingredientName, setIngredientName] = useState("") 
-    const [isDisabled, setIsDisabled] = useState(false) 
- 
-    // Create an Ingredient 
-    const { mutate } = useMutation( 
-        async (ingredientName: string) => await axios.post('/api/ingredients/addIngredient', { ingredientName }),
-        {
-            onError: (error) => {
-                console.log(error)
-            },
-            onSuccess: (data) => {
-                setIngredientName("")
-                setIsDisabled(false)
-            }
-        }
-    )
+"use client";
 
-    const submitIngredient = async (e: React.FormEvent) => {
-        e.preventDefault()
-        setIsDisabled(true)
-        mutate(ingredientName)
+import { useState } from "react";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import axios from "axios";
+
+export default function AddIngredient() {
+  const [ingredientName, setIngredientName] = useState("");
+  const [isDisabled, setIsDisabled] = useState(false);
+
+  // Create an Ingredient
+  const { mutate } = useMutation(
+    async (ingredientName: string) =>
+      await axios.post("/api/ingredients/addIngredient", { ingredientName }),
+    {
+      onError: (error) => {
+        console.log(error);
+      },
+      onSuccess: (data) => {
+        setIngredientName("");
+        setIsDisabled(false);
+      },
     }
+  );
 
-    return( 
-        <form onSubmit={submitIngredient} className="bg-white my-8 p-8 rounded-md"> 
-            <div className='flex flex-col my-4'> 
-                <textarea onChange={(e) => setIngredientName(e.target.value)} 
-                name="ingredientName"  
-                value={ingredientName} 
-                placeholder="canned tomato" 
-                className='p-4 text-lg rounded-md my-2  bg-gray-200' 
-                ></textarea> 
-            </div> 
-            <div> 
-                <button 
-                    disabled={isDisabled} 
-                    className="text-sm bg-teal-600 text-white py-2 px-6 rounded-xl disabled:opacity-25" 
-                    type="submit" 
-                > 
-                    Add an Ingredient 
-                </button> 
-            </div> 
-        </form> 
-    ) 
+  const submitIngredient = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setIsDisabled(true);
+    mutate(ingredientName);
+  };
+
+  return (
+    <form onSubmit={submitIngredient} className="bg-white my-8 p-8 rounded-md">
+      <div className="flex flex-col my-4">
+        <label className="block text-gray-700 text-lg font-bold mb-2 p-2">
+          Ingredient Name
+        </label>
+        <input
+          onChange={(e) => setIngredientName(e.target.value)}
+          name="ingredientName"
+          value={ingredientName}
+          placeholder="canned tomato"
+          className="p-4 text-lg rounded-md my-2  bg-gray-200"
+        ></input>
+      </div>
+      <div>
+        <button
+          disabled={isDisabled}
+          className="text-sm bg-teal-600 text-white py-2 px-6 rounded-xl disabled:opacity-25"
+          type="submit"
+        >
+          Add an Ingredient
+        </button>
+      </div>
+    </form>
+  );
 }
